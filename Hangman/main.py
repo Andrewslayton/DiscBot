00cfdabd504b1304e19ced972441dc67888d1dc1
Andrewslayton <andrewsla1726@gmail.com>
2023-12-27 04:07:21 -0500

    hangman final product

--- Hangman/main.py
+++ Hangman/main.py
@@ -3,6 +3,10 @@ from discord.ext import commands
 import base64
 from dotenv import load_dotenv
 import os 
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN2')
@@ -17,6 +21,19 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='#', intents=intents)
 hangman_game = {}
 
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    discord_id = Column(String, unique=True)
+    score = Column(Integer, default=0)
+
+engine = create_engine('sqlite:///hangman.db')
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
 
 @bot.command()
 async def start(ctx, *, word):
@@ -25,7 +42,9 @@ async def start(ctx, *, word):
     hangman_game[ctx.guild.id] = {
         "word": word.lower(),
         "guessed": [],
-        "lives": 6
+        "lives": 6,
+        "creator_id": ctx.author.id,
+        "current_status": ["_"] * len(word)
     }
     length = len(word)
     await ctx.send("Welcome to Hangman! The word is " + str(length) + " letters long" + ". Guess a letter by typing #guess <letter>")
@@ -48,12 +67,17 @@ async def guess(ctx, *, letter):
     word = game["word"]
     guessed = game["guessed"]
     lives = game["lives"]
+    creator = game["creator_id"]
+    if ctx.author.id == creator:
+        await ctx.send("You made the word silly")
+        return
     if letter in guessed:
         await ctx.send("You already guessed that letter")
         return
     guessed.append(letter)
     if letter in word:
         await ctx.send("Correct!")
+        update_score(ctx.author.id, 10)
     else:
         await ctx.send("Wrong!")
         lives -= 1
@@ -67,6 +91,43 @@ async def guess(ctx, *, letter):
         await ctx.send("You won! The word was " + word)
         del hangman_game[ctx.guild.id]
         return
+    for i in range(len(word)):
+        if word[i] == letter:
+            game["current_status"][i] = letter
+    await ctx.send("Current status: " + "".join(game["current_status"]))
+    
+    
+@bot.command()
+async def leaderboard(ctx):
+    session = Session()
+    all_users = session.query(User).order_by(User.score.desc()).all()
+    if not all_users:
+        await ctx.send("No scores available.")
+    else:
+        leaderboard_message = "🏆 Hangman Leaderboard 🏆\n"
+        for rank, user in enumerate(all_users, start=1):
+            member = ctx.guild.get_member(int(user.discord_id))
+            if member:
+                username = member.name
+            else:
+                username = "Unknown User"
+            leaderboard_message += f"{rank}. {username} - {user.score} points\n"
+        await ctx.send(leaderboard_message)
+    session.close()
+    
+def update_score(discord_id, points):
+    session = Session()
+    user = session.query(User).filter_by(discord_id=str(discord_id)).first()
+
+    if not user:
+        user = User(discord_id=str(discord_id), score=points)
+        session.add(user)
+    else:
+        user.score += points
+
+    session.commit()
+    session.close()
+
 bot.run(BOT_TOKEN)
 
 # def check(message):
